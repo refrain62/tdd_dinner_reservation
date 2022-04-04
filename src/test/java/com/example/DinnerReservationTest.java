c5a01949292d8d4e91d058793f1c755b7838c586
nao <nao_h13@hotmail.com>
2022-04-05 00:11:34 +0900

    クーポン適用テスト追加

--- src/test/java/com/example/DinnerReservationTest.java
+++ src/test/java/com/example/DinnerReservationTest.java
@@ -19,7 +19,7 @@ public class DinnerReservationTest
         // 人数 - 10人
         reserv.setUser( 10 );
 
-        // コース: 1:松
+        // コース: 松
         reserv.setCourse( DinnerReservation.Course.Matsu );
 
         // 結果 - 料金 = 70,000 円(7,000円 × 10名)
@@ -37,7 +37,7 @@ public class DinnerReservationTest
         // 人数 - 10人
         reserv.setUser( 10 );
 
-        // コース: 2:竹
+        // コース: 竹
         reserv.setCourse( DinnerReservation.Course.Take);
 
         // 結果 - 料金 = 50,000 円(5,000円 × 10名)
@@ -55,10 +55,32 @@ public class DinnerReservationTest
         // 人数 - 10人
         reserv.setUser( 10 );
 
-        // コース: 3:梅
+        // コース: 梅
         reserv.setCourse( DinnerReservation.Course.Ume );
 
         // 結果 - 料金 = 30,000 円(3,000円 × 10名)
         assertEquals( 30000, reserv.getCharget() );
+    }
+    
+    /**
+     * クーポン適用テスト
+     */
+    @Test
+    public void testCoupon()
+    {
+        DinnerReservation reserv = new DinnerReservation();
+
+        // 人数 - 10人
+        reserv.setUser( 10 );
+
+        // コース: 松
+        reserv.setCourse( DinnerReservation.Course.Matsu );
+
+        // クーポン適用 - 1枚
+        reserv.setCoupon( 1 );
+
+        // 結果 - 料金 = 60,000 円(7,000円 × 10名 - 10,000円<クーポン>)
+        assertEquals( 60000, reserv.getCharget() );
+
     }
 }
